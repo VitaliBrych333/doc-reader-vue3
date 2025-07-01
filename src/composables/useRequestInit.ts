8620e46cea18a410161ba5183322c0c85645367c
Vitali Brych <hdhmf@mail.ru>
2025-07-02 01:25:00 +0300

    refactor: add links

--- src/composables/useRequestInit.ts
+++ src/composables/useRequestInit.ts
@@ -2,7 +2,7 @@
 export function useRequestInit(method: string, body: object | null = null, convertToJson = false) {
   const init: RequestInit = {
     method,
-    headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': 'https://doc-reader.leapcell.app' },
+    headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': 'https://doc-reader-vue3.netlify.app' },
     credentials: 'include' // "include" | "omit" | "same-origin" - to get in response a cookie with token and set in the browser
   };
 
