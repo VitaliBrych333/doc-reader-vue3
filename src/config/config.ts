d0f840a5392726d31ddf514c1c87c24bae6baec5
Vitali Brych <hdhmf@mail.ru>
2025-07-02 18:33:59 +0300

    fix

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,4 @@
 export enum CONFIG {
-  BASE_URL = 'https://doc-reader.leapcell.app'  //  to local run - 'http://localhost:8080'
+  // BASE_URL = 'https://doc-reader.leapcell.app'  //  to local run - 'http://localhost:8080'
+  BASE_URL = 'https://deploy-doc-reader-go-servercom-production.up.railway.app'
 }
